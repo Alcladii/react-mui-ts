2e29c33be1b3f1778f8e34c212113f16ccced0b5
Alcladii <122701312+Alcladii@users.noreply.github.com>
2025-08-05 14:27:44 +0100

    first commit

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,12 @@
+import {Button} from '@mui/material';
+
+export const MuiButton = () => {
+  return (
+    <div>
+    <div><Button variant='text'>Text</Button></div>
+    <div><Button variant='contained'>Contained</Button></div>
+    <div><Button variant='outlined'>Outlined</Button></div>
+    </div>
+    
+  )
+}
