2ad3599c526eae89b04e18083261b4500a3b7776
Alcladii <122701312+Alcladii@users.noreply.github.com>
2025-08-07 14:03:23 +0100

    checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { MuiTypography } from './components/MuiTypography';
-import { MUIButton } from './components/MUIButton';
-import { MUITextField } from './components/MUITextField'
-import { MUISelect } from './components/MUISelect';
-import {MUIRadioButton} from './components/MUIRadioButton';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MuiTypography } from "./components/MuiTypography";
+import { MUIButton } from "./components/MUIButton";
+import { MUITextField } from "./components/MUITextField";
+import { MUISelect } from "./components/MUISelect";
+import { MUIRadioButton } from "./components/MUIRadioButton";
+import { MUICheckbox } from "./components/MUICheckbox"
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
-      {/* <MUITextField /> */}      
+      {/* <MUITextField /> */}
       {/* <MUITextField /> */}
       {/* <MUISelect /> */}
-      {<MUIRadioButton />}
+      {/* {<MUIRadioButton />} */}
+      <MUICheckbox />
     </div>
   );
 }
